4ed9d9984e2bdb39556fd900dcf8c63ca098b2be
Roby Renata <robyrenata@gmail.com>
2018-09-26 10:52:50 +0700

    Initial commit

---
+++ Unity 3D/Assets/_RES/res-code/menu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour {
+
+	public void GoToMainMenu(){
+		Application.LoadLevel("2-menu");
+	}
+
+	public void GoToARCamera(){
+		Application.LoadLevel("3-arcamera");
+	}
+	
+	public void GoToVideo(){
+		Application.LoadLevel("4-video");
+	}
+	
+	public void GoToManual(){
+		Application.LoadLevel("5-manual");
+	}
+	
+	public void GoToAbout(){
+		Application.LoadLevel("6-about");
+	}
+
+	public void ExitApplication(){
+		Application.Quit ();
+	}
+	
+	public void ContactClick(){
+		Application.OpenURL("tel://+6289680001588");
+	}
+	
+	public void LocationClick(){
+		Application.OpenURL("https://goo.gl/maps/XfePJ5PN4W22");
+	}
+	
+	public void MarkerDownloadClick(){
+		Application.OpenURL("https://drive.google.com/open?id=1n9MqLSlCdzsG7cmNqgtsrK4ARMD5zle2");
+	}
+	
+	public void AppDownloadClick(){
+		Application.OpenURL("https://play.google.com/store?hl=en");
+	}
+
+}
